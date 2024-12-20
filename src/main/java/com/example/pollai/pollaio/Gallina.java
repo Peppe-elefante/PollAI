6f3eb9f0133118be35156a18b281570b69a81419
Peppe-elefante <giuseppeelefante123@gmail.com>
2024-12-20 15:23:21 +0100

    Creazione Entity Pollaio e Gallina

---
+++ src/main/java/com/example/pollai/pollaio/Gallina.java
@@ -0,0 +1,80 @@
+package com.example.pollai.pollaio;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "gallina")
+public class Gallina implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String razza;
+
+    @Column(nullable = false)
+    private int eta;
+
+    @Column(nullable = false)
+    private int peso;
+
+    @ManyToOne
+    @JoinColumn(name = "pollaio_id", nullable = false)
+    private Pollaio pollaio;
+
+    public Gallina() {
+    }
+
+    // Parameterized constructor
+    public Gallina(String razza, int eta, int peso, Pollaio pollaio) {
+        this.razza = razza;
+        this.eta = eta;
+        this.peso = peso;
+        this.pollaio = pollaio;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRazza() {
+        return razza;
+    }
+
+    public void setRazza(String razza) {
+        this.razza = razza;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public Pollaio getPollaio() {
+        return pollaio;
+    }
+
+    public void setPollaio(Pollaio pollaio) {
+        this.pollaio = pollaio;
+    }
+
+
+}
---
+++ src/main/java/com/example/pollai/pollaio/Pollaio.java
@@ -0,0 +1,61 @@
+package com.example.pollai.pollaio;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "pollaio")
+public class Pollaio implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    @OneToMany(mappedBy = "pollaio", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Gallina> galline;
+
+    public Pollaio() {
+    }
+
+    public Pollaio(int quantity, List<Gallina> galline) {
+        this.quantity = galline.size();
+        this.galline = galline;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<Gallina> getGalline() {
+        return galline;
+    }
+
+    public void setGalline(List<Gallina> galline) {
+        this.galline = galline;
+    }
+
+    public void addGallina(Gallina gallina) {
+        galline.add(gallina);
+        gallina.setPollaio(this);
+    }
+
+    public void removeGallina(Gallina gallina) {
+        galline.remove(gallina);
+        gallina.setPollaio(null);
+    }
+
+
+}
