c98f2e78fb46a7aecc86b8b5de02de491d9812bf
Peppe-elefante <giuseppeelefante123@gmail.com>
2025-01-04 17:11:38 +0100

    commit su branch produzione: Implementato la funzionalita di vedere le uova prodotte negli ultimi 3 mesi, 1 mese e 1 anno

--- src/main/java/com/example/pollai/produzione/ProduzioneController.java
+++ src/main/java/com/example/pollai/produzione/ProduzioneController.java
@@ -8,9 +8,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 
@@ -67,6 +69,38 @@ public class ProduzioneController {
         utente.setPollaio(pollaio);
         session.setAttribute("user", utente);
         // Reindirizza alla pagina precedente o alla home
+        return "redirect:" + (referer != null ? referer : "/");
+    }
+
+    @GetMapping("/archivio-produzione")
+    public String archivioProduzione(@RequestParam String category, HttpSession session, HttpServletRequest request, RedirectAttributes redirectAttributes){
+        LocalDate data = LocalDate.now();
+        String referer = request.getHeader("Referer");
+
+        Utente sessionUser = (Utente) session.getAttribute("user");
+        Utente utente = utenteService.getUtenteById(sessionUser.getId())
+                .orElseThrow(() -> new RuntimeException("Utente non trovato nel database"));
+
+        Pollaio pollaio = utente.getPollaio();
+        DatiProduzione produzione = pollaio.getProduzione();
+        switch (category) {
+            case "3 months":
+                data = data.minusMonths(3);
+                break;
+            case "1 month":
+                data = data.minusMonths(1);
+                break;
+
+            default:
+                data = data.minusYears(1);
+        }
+
+        String archivio = "The eggs produced in the last " + category + " are ";
+        int eggs = produzioneService.getProduzionePeriodo(data, produzione);
+        archivio += eggs;
+
+        redirectAttributes.addFlashAttribute("archivio", archivio);
+
         return "redirect:" + (referer != null ? referer : "/");
     }
 
