e5ee8e560de7a2ee527887bf63e9a28b1a77e264
Lucia <l.pisacane3@studenti.unisa.it>
2025-01-05 21:09:27 +0100

    mapping, html e css per: ourStory, ourSolutions, privacy; css login e creazione pagina di errore

--- src/main/java/com/example/pollai/MainController.java
+++ src/main/java/com/example/pollai/MainController.java
@@ -33,4 +33,23 @@ public class MainController {
         model.addAttribute("user", new Utente());
         return "registeruser";
     }
+
+    // Mapping verso la pagina Our Story
+    @GetMapping("/our-story")
+    public String ourStory() {
+        return "our-story";
+    }
+
+    // Mapping verso la pagina Our Solutions
+    @GetMapping("/our-solutions")
+    public String ourSolutions() {
+        return "our-solutions";
+    }
+
+    // Mapping verso la pagina Privacy Policy
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
+    }
+
 }
